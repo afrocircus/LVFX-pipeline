5e6ae96bc80133f8ab89b21cd685d7fa56dae176
afrocircus <natasha.kelkar@gmail.com>
2016-05-26 11:41:42 +0200

    Moving ftrack events folder

---
+++ ftrack-connect-plugins/custom_events/task_folder_create.py
@@ -0,0 +1,59 @@
+import ftrack
+import os
+import sys
+import shutil
+
+def getSequence(task):
+    sq = ''
+    for parent in task.getParents():
+        if 'objecttypename' in parent.keys():
+            if parent.get('objecttypename') == 'Sequence':
+                sq = parent.getName()
+    return sq
+
+
+def copyTemplateFiles(templateFolder, task, taskFolder):
+    for file in os.listdir(templateFolder):
+        filepath = os.path.join(templateFolder, file)
+        if os.path.isfile(filepath):
+            if task in file:
+                newFilepath = os.path.join(taskFolder, file)
+                if not os.path.exists(newFilepath):
+                    shutil.copy(filepath, newFilepath)
+
+
+def callback(event):
+    """ This plugin creates a template folder structure on disk.
+    """
+    for entity in event['data'].get('entities', []):
+        if entity.get('entityType') == 'task' and entity['action'] == 'add':
+            task = ftrack.Task(id=entity['entityId'])
+            taskName = task.getName().lower()
+            if task.getObjectType() == 'Task':
+                project = task.getProject()
+                sq = getSequence(task)
+                shotName = task.getParent().getName()
+                if sq == '':
+                    return
+                projFolder = project.get('root')
+                if projFolder == '':
+                    disk = ftrack.Disk(project.get('diskid'))
+                    rootFolder = ''
+                    if sys.platform == 'win32':
+                        rootFolder = disk.get('windows')
+                    elif sys.platform == 'linux2':
+                        rootFolder = disk.get('unix')
+                    projFolder = os.path.join(rootFolder, project.getName())
+                shotsFolder = os.path.join(projFolder, 'shots')
+                templateFolder = os.path.join(projFolder, 'template_files')
+                sqFolder = os.path.join(shotsFolder, sq)
+                shotFolder = os.path.join(sqFolder, shotName)
+                sceneFolder = os.path.join(shotFolder, 'scene')
+                taskFolder = os.path.join(sceneFolder, taskName)
+                if not os.path.exists(taskFolder):
+                    os.makedirs(taskFolder)
+                copyTemplateFiles(templateFolder, taskName, taskFolder)
+
+
+# Subscribe to events with the update topic.
+ftrack.EVENT_HUB.subscribe('topic=ftrack.update', callback)
