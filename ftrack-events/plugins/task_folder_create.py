93fe883eb55c7aae95f296f3cde7691154f82e1f
afrocircus <natasha.kelkar@gmail.com>
2016-06-10 15:49:28 +0200

    handles asset task creation

--- ftrack-events/plugins/task_folder_create.py
+++ ftrack-events/plugins/task_folder_create.py
@@ -12,6 +12,19 @@ def getSequence(task):
     return sq
 
 
+def getProjectFolder(project):
+    projFolder = project.get('root')
+    if projFolder == '':
+        disk = ftrack.Disk(project.get('diskid'))
+        rootFolder = ''
+        if sys.platform == 'win32':
+            rootFolder = disk.get('windows')
+        elif sys.platform == 'linux2':
+            rootFolder = disk.get('unix')
+        projFolder = os.path.join(rootFolder, project.getName())
+    return projFolder
+
+
 def copyTemplateFiles(templateFolder, task, taskFolder, shotName):
     taskName = task.getName()
     for file in os.listdir(templateFolder):
@@ -27,6 +40,18 @@ def copyTemplateFiles(templateFolder, task, taskFolder, shotName):
                 }
                 task.setMeta(metadata)
 
+def createAssetFolders(task, projectFolder):
+    taskName = task.getName().lower()
+    parents = task.getParents()
+    parents.reverse()
+    for parent in parents:
+        if 'objecttypename' in parent.keys():
+            projectFolder = os.path.join(projectFolder, parent.getName().lower())
+    taskFolder = os.path.join(projectFolder, taskName)
+    if not os.path.exists(taskFolder):
+        os.makedirs(taskFolder)
+    return taskFolder
+
 
 def callback(event):
     """ This plugin creates a template folder structure on disk.
@@ -37,27 +62,24 @@ def callback(event):
             taskName = task.getName().lower()
             if task.getObjectType() == 'Task':
                 project = task.getProject()
-                sq = getSequence(task)
-                shotName = task.getParent().getName()
-                if sq == '':
-                    return
-                projFolder = project.get('root')
-                if projFolder == '':
-                    disk = ftrack.Disk(project.get('diskid'))
-                    rootFolder = ''
-                    if sys.platform == 'win32':
-                        rootFolder = disk.get('windows')
-                    elif sys.platform == 'linux2':
-                        rootFolder = disk.get('unix')
-                    projFolder = os.path.join(rootFolder, project.getName())
-                shotsFolder = os.path.join(projFolder, 'shots')
+                projFolder = getProjectFolder(project)
+                if task.getParent().get('objecttypename') == 'Asset Build':
+                    taskFolder = createAssetFolders(task, projFolder)
+                    shotName = task.getParent().getName().lower()
+                    shotName = '{0}_{1}'.format(shotName, taskName)
+                else:
+                    sq = getSequence(task)
+                    shotName = task.getParent().getName()
+                    if sq == '':
+                        return
+                    shotsFolder = os.path.join(projFolder, 'shots')
+                    sqFolder = os.path.join(shotsFolder, sq)
+                    shotFolder = os.path.join(sqFolder, shotName)
+                    sceneFolder = os.path.join(shotFolder, 'scene')
+                    taskFolder = os.path.join(sceneFolder, taskName)
+                    if not os.path.exists(taskFolder):
+                        os.makedirs(taskFolder)
                 templateFolder = os.path.join(projFolder, 'template_files')
-                sqFolder = os.path.join(shotsFolder, sq)
-                shotFolder = os.path.join(sqFolder, shotName)
-                sceneFolder = os.path.join(shotFolder, 'scene')
-                taskFolder = os.path.join(sceneFolder, taskName)
-                if not os.path.exists(taskFolder):
-                    os.makedirs(taskFolder)
                 copyTemplateFiles(templateFolder, task, taskFolder, shotName)
 
 
