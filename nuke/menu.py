ca6b48dc996d18d755896472702c59db87d8449e
afrocircus <natasha.kelkar@gmail.com>
2016-06-14 17:29:07 +0200

    added menu items for start and stop ftrack timer

--- nuke/menu.py
+++ nuke/menu.py
@@ -12,15 +12,18 @@ from scripts import writeNodeManager
 from scripts import readNodeManager
 from scripts import slate
 from scripts import easySave
+from scripts import ftrackTimer
 
 
 def addCustomPanel1():
     pythonPanel = panels.registerWidgetAsPanel('submitUI.ShotSubmitUI', 'Shot Submit', 'locovfx.shotSubmit.plugin', True)
     return pythonPanel.addToPane()
 
+
 def addCustomPanel2():
     pythonPanel = panels.registerWidgetAsPanel('nukeProRes.NukeProResWindow', 'ProRes Tool', 'locovfx.prores.plugin', True)
     return pythonPanel.addToPane()
+
 
 nuke.pluginAddPath('')
 nuke.menu('Nuke').addCommand('File/LVFX-Save New Version', easySave.save, 'Alt+Shift+S')
@@ -29,9 +32,13 @@ toolbar = menubar.addMenu("LocoVFX")
 m = toolbar.addMenu('gizmos')
 m.addCommand("Slate", "nuke.createNode(\"slate\")", icon="V_Slate.png")
 nuke.menu('Nuke').addCommand('LocoVFX/Shot Submit', addCustomPanel1)
+nuke.menu('Nuke').addCommand('LocoVFX/Start Ftrack Timer', ftrackTimer.startTimer)
+nuke.menu('Nuke').addCommand('LocoVFX/Stop Ftrack Timer', ftrackTimer.stopTimer)
 panels.registerWidgetAsPanel('submitUI.ShotSubmitUI', 'Shot Submit', 'locovfx.shotSubmit.plugin')
 nuke.menu('Nuke').addCommand('LocoVFX/ProRes Tool', addCustomPanel1)
 panels.registerWidgetAsPanel('nukeProRes.NukeProResWindow', 'ProRes Tool', 'locovfx.prores.plugin')
 toolbar = nuke.toolbar("Nodes")
 toolbar.addMenu("VideoCopilot", icon="VideoCopilot.png")
 toolbar.addCommand( "VideoCopilot/OpticalFlares", "nuke.createNode('OpticalFlares')", icon="OpticalFlares.png")
+
+nuke.addOnScriptClose(ftrackTimer.stopTimer)
