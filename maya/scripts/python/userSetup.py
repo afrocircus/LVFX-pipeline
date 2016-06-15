7083bfc341c7d01f58d38277a5d080d134108f56
afrocircus <natasha.kelkar@gmail.com>
2016-06-15 18:11:59 +0200

    added callback to stop ftrack user timer when maya exits.

--- maya/scripts/python/userSetup.py
+++ maya/scripts/python/userSetup.py
@@ -1,9 +1,33 @@
 import maya.cmds as mc
+import ftrack_api
+import os
 
-print "loading custom plugins ..."
+from maya.utils import executeDeferred
+
+print "loading custom plugins! ..."
 
 def loadAndInit():
     mc.loadPlugin('shotSubmitPlugin.py', quiet=True)
     mc.spLoadSubmitPlugin()
 
+
+def onClose():
+    print "Maya closing..."
+    if 'FTRACK_TASKID' in os.environ.keys():
+        session = ftrack_api.Session(
+            server_url=os.environ['FTRACK_SERVER'],
+            api_user=os.environ['FTRACK_API_USER'],
+            api_key=os.environ['FTRACK_API_KEY']
+        )
+        task = session.query('Task where id is %s' % os.environ['FTRACK_TASKID'])
+        user = session.query('User where username is %s' % os.environ['FTRACK_API_USER']).one()
+        user.stop_timer()
+
+
+def addQuitAppCallback():
+    print "addQuitAppCallback called"
+    mc.scriptJob(e=["quitApplication", "onClose()"])
+
+
 mc.evalDeferred("loadAndInit()")
+executeDeferred("addQuitAppCallback()")
