a19f710b97713326ed84e1d1132239dc6c929ada
afrocircus <natasha.kelkar@gmail.com>
2016-07-20 15:23:13 +0200

    thumbnail bug fix

--- ftrack-connect-plugins/custom_hook/reference_upload.py
+++ ftrack-connect-plugins/custom_hook/reference_upload.py
@@ -90,7 +90,7 @@ class RefClipUploader(ftrack.Action):
     def prepMediaFiles(self, outputFile):
         outfilemp4 = os.path.splitext(outputFile)[0] + '.mp4'
         outfilewebm = os.path.splitext(outputFile)[0] + '.webm'
-        thumbnail = os.path.join(os.path.split(outputFile)[0], 'thumbnail.png')
+        thumbnail = os.path.splitext(outputFile)[0]+ '_thumbnail' + '.png'
         metadata = {
             'source_file': outputFile
             }
