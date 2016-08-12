1f72d81680abfd32d1374800e19fc90d829d1d96
afrocircus <natasha.kelkar@gmail.com>
2016-08-12 11:40:40 +0200

    removed hard coded values

--- maya/scripts/python/vrayServerRestart.py
+++ maya/scripts/python/vrayServerRestart.py
@@ -5,12 +5,13 @@ import pymel.core as pm
 serverConfig = '/data/production/pipeline/linux/common/config/server.json'
 jsonFile = open(serverConfig).read()
 data = json.loads(jsonFile)
-password = data['password']
 
-cmd = 'sshpass -p "%s" ssh root@192.168.0.208 ' \
-      '/usr/ChaosGroup/VRLService/linux_x64/bin/vrlsvcctl restart' % password
+cmd = 'sshpass -p "%s" ssh %s@%s ' \
+      '/usr/ChaosGroup/VRLService/linux_x64/bin/vrlsvcctl restart' % (data['password'], data['user'],
+                                                                      data['host'])
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 outputStr = process.stdout.readlines()
+print outputStr
 if outputStr:
     if 'Starting V-Ray license service...done\n' in outputStr:
         pm.confirmDialog(t= 'Server Restart', m = 'Vray Server Successfully Restarted!', b='OK')
