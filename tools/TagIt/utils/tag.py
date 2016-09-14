8ed650e70b682530fcf9585d332049ae3677b6ee
afrocircus <natasha.kelkar@gmail.com>
2016-09-14 17:47:36 +0200

    selected text is description of shot/scene

--- tools/TagIt/utils/tag.py
+++ tools/TagIt/utils/tag.py
@@ -17,23 +17,29 @@ class Tag(QtGui.QMenu):
         :return: None
         """
         self.clear()
+        currentPage = self.findPage()
         sceneAction = QtGui.QAction('New Scene', self)
-        sceneAction.triggered.connect(self.onSceneTriggered)
+        sceneAction.triggered.connect(lambda arg=currentPage: self.onSceneTriggered(arg))
         self.addAction(sceneAction)
 
         shotMenu = QtGui.QMenu()
         shotMenu.setTitle('New Shot in')
         for scene in sorted(self.tagDict.keys()):
             shotAction = QtGui.QAction(scene, self)
-            shotAction.triggered.connect(lambda arg=shotAction: self.onShotTriggered(arg))
+            shotAction.triggered.connect(lambda arg=(shotAction,currentPage):self.onShotTriggered((arg)))
             shotMenu.addAction(shotAction)
         self.addMenu(shotMenu)
 
-        descAction = QtGui.QAction('Description', self)
-        descAction.triggered.connect(self.onSceneTriggered)
-        self.addAction(descAction)
+        '''shotSceneList = self.getRelevantShots(currentPage)
+        descMenu = QtGui.QMenu()
+        descMenu.setTitle('Description for')
+        for scene in sorted(shotSceneList):
+            descAction = QtGui.QAction(scene, self)
+            descAction.triggered.connect(lambda arg=descAction: self.onDescTriggered(arg))
+            descMenu.addAction(descAction)
+        self.addMenu(descMenu)'''
 
-    def onSceneTriggered(self):
+    def onSceneTriggered(self, currentPage):
         """
         Slot triggered when scene action signal triggered.
         Creates/Updates the scene information in tagDict
@@ -45,13 +51,15 @@ class Tag(QtGui.QMenu):
         else:
             newScene = '001'
         d = self.tagDict[newScene]
-        d['page'] = self.findPage()
+        d['page'] = currentPage
         d['shots'] = defaultdict(dict)
+        d['description'] = self.parent.textCursor().selectedText()
 
-    def onShotTriggered(self, action):
+    def onShotTriggered(self, (action, currentPage)):
         """
         Slot triggered when shot action signal triggered.
         Creates/Updates the shot information in tagDict
+        :param action: Action that has been selected.
         :return: None
         """
         scene = action.text()
@@ -64,7 +72,11 @@ class Tag(QtGui.QMenu):
             newShot = '0010'
         shotName = '%s_%s' % (scene, newShot)
         d = shotsDict[shotName]
-        d['page'] = self.findPage()
+        d['page'] = currentPage
+        d['description'] = self.parent.textCursor().selectedText()
+
+    def onDescTriggered(self, action):
+        print action.text()
 
     def findPage(self):
         """
@@ -73,14 +85,16 @@ class Tag(QtGui.QMenu):
         """
         # Grab the parent's cursor
         cursor = self.parent.textCursor()
+        allText = self.parent.toPlainText()
+
 
         # Grab the text from start of document to end of selection.
-        start = 0
+        #start = 0
         end = cursor.selectionEnd()
-        cursor.setPosition(start)
-        cursor.movePosition(QtGui.QTextCursor.Right, QtGui.QTextCursor.KeepAnchor, end-start)
-        self.parent.setTextCursor(cursor)
-        text = cursor.selectedText()
+        #cursor.setPosition(start)
+        #cursor.movePosition(QtGui.QTextCursor.Right, QtGui.QTextCursor.KeepAnchor, end-start)
+        #self.parent.setTextCursor(cursor)
+        text = allText[:end]
 
         # Page numbers should be of the format "Page 1"
         query = 'Page ' + r'\d+'
@@ -96,3 +110,16 @@ class Tag(QtGui.QMenu):
             if page:
                 pageNo = int(page[-1])
         return pageNo
+
+    def getRelevantShots(self, currentPage):
+        # Scenes that are relevant to the current page
+        relevantList = [item[0] for item in self.tagDict.iteritems() if item[1]['page'] <= currentPage]
+
+        # Shots that are relevant to the current page
+        shotDicts = [item['shots'] for item in self.tagDict.itervalues()]
+        for shotDict in shotDicts:
+            shotList = [item[0] for item in shotDict.iteritems() if item[1]['page'] <= currentPage]
+            relevantList.extend(shotList)
+        return relevantList
+
+
