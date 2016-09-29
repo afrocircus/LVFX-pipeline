2c00ed666fe4443855a141521231936bef9bb56f
Rohan Bhasin <rohanbhasin@Rohans-MacBook-Pro.local>
2016-09-29 17:27:21 +0200

    cxfreeze setup script

---
+++ tools/TagIt/setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('tag_it.py', base=base)
+]
+
+setup(name='TagIt',
+      version = '1.0',
+      description = 'Tags a movie script',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
