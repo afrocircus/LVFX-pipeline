e794db93421da37e99af1227417b5bf07744fd85
Natasha Kelkar <natasha.kelkar@gmail.com>
2016-09-29 17:40:26 +0200

    cxfreeze setup

--- tools/TagIt/setup.py
+++ tools/TagIt/setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = [])
+buildOptions = dict(packages = ['utils','lxml._elementpath'], excludes = [])
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
