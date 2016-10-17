a1cd11541cfb36a61843cac8e0b59074a5aa32df
afrocircus <natasha.kelkar@gmail.com>
2016-10-17 14:14:52 +0200

    Pyblish for rigging

---
+++ pyblish-plugins/rigging/extract_rig.py
@@ -0,0 +1,42 @@
+import os
+import ftrack
+import pyblish.api
+from maya import cmds
+from pyblish_maya import maintained_selection
+
+
+@pyblish.api.log
+class ExtractRig(pyblish.api.InstancePlugin):
+
+    families = ['scene']
+    label = 'Extract Rig'
+    hosts = ['maya']
+    order = pyblish.api.ExtractorOrder
+
+
+    def process(self, instance):
+
+        node = instance.data['rig']
+        versionDir = instance.data['vprefix'] + instance.data['version']
+        publishDir = os.path.join(instance.data['publishDir'], versionDir)
+        if not os.path.exists(publishDir):
+            os.makedirs(publishDir)
+
+        metadata = instance.data['metadata']
+        metadata['version'] = versionDir
+
+        task = ftrack.Task(instance.context.data['taskid'])
+        assetName = task.getParent().getName().lower()
+
+        assetFile = os.path.join(publishDir, '%s_rig.mb' % assetName)
+        metadata['publish_rig'] = assetFile
+
+        with maintained_selection():
+            cmds.select(node, noExpand=True)
+            cmds.file(assetFile,
+                      force=True,
+                      typ='mayaBinary',
+                      exportSelected=True,
+                      preserveReferences=False,
+                      constructionHistory=True)
+        self.log.info('Extraction completed successfully')
---
+++ pyblish-plugins/rigging/validate_rig.py
@@ -0,0 +1,36 @@
+import pyblish.api
+import pymel.core
+from maya import cmds
+
+
+@pyblish.api.log
+class ValidateRig(pyblish.api.InstancePlugin):
+
+    order = pyblish.api.ValidatorOrder
+    hosts = ['maya']
+    families = ['scene']
+    label = 'Validate Rig'
+
+    def process(self, instance):
+
+        assemblies = pymel.core.ls(assemblies=True)
+        rigInstances = []
+        for each in assemblies:
+            if any(ext in each.name().lower() for ext in ['grp']):
+                rigInstances.append(each.name())
+
+        if len(rigInstances) != 1:
+            self.log.error('There should be only one parent group in the scene')
+            raise pyblish.api.ValidationError
+
+        rigMembers = cmds.listRelatives(rigInstances[0], children=True)
+        rigs = []
+        for each in rigMembers:
+            if any(ext in each for ext in ['geo', 'rig']):
+                rigs.append(each)
+
+        if len(rigs) != 2:
+            self.log.error('There should be 2 child groups called geo and rig')
+            raise pyblish.api.ValidationError
+
+        instance.set_data('rig', value=rigInstances[0])
