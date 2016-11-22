3666f875133b358f21e8e5d518a43a95d30e252c
afrocircus <natasha.kelkar@gmail.com>
2016-11-22 12:43:39 +0200

    adds HUD to playblast

--- pyblish-plugins/extract_playblast.py
+++ pyblish-plugins/extract_playblast.py
@@ -2,7 +2,9 @@ import os
 import pyblish.api
 import ftrack
 import json
+import datetime
 from Utils import capture
+import maya.cmds as cmds
 
 
 def load_preset(path):
@@ -33,6 +35,13 @@ class ExtractPlayblast(pyblish.api.InstancePlugin):
             os.makedirs(playblastDir)
         version = instance.data['vprefix'] + instance.data['version']
         shotName = task.getParent().getName()
+
+        users = [user.get('firstname') for user in task.getUsers()]
+        usernames = ', '.join(users)
+        projectName = task.getProject().getName()
+        self.shotInfo = '{0} | {1} | {2} | {3} | {4}'.format(projectName, shotName,
+                                                             taskName, version, usernames)
+
         outFile = os.path.join(playblastDir, '{0}_{1}_playblast.mov'.format(shotName, version))
         instance.set_data('playblastFile', value=outFile)
 
@@ -44,8 +53,40 @@ class ExtractPlayblast(pyblish.api.InstancePlugin):
         preset = load_preset(presetPath)
         preset['camera'] = 'renderCam'
 
+        self.addHUD()
+
         capture.capture(filename=outFile,
                         overwrite=True,
-                        quality=70,
+                        quality=70, compression='jpeg',
                         **preset)
+        self.removeHUD()
+
         self.log.info('extract playblast')
+
+    def getShotInfo(self, *args):
+        shotInfo = self.shotInfo
+        return shotInfo
+
+    def getDate(self, *args):
+        today = datetime.datetime.today()
+        dateStr = '%02d-%02d-%d' % (today.day, today.month, today.year)
+        return dateStr
+
+    def getFrame(self, *args):
+        frameCount = cmds.currentTime(q=True)
+        return frameCount
+
+    def addHUD(self):
+        self.removeHUD()
+        cmds.displayColor('headsUpDisplayLabels', 18, dormant=True)
+        cmds.headsUpDisplay('leftTopHUD', section=0, block=1, blockSize='medium', dataFontSize='large',
+                       command=self.getShotInfo, atr=True)
+        cmds.headsUpDisplay('rightTopHUD', section=4, block=1, blockSize='medium', dataFontSize='large',
+                       command=self.getDate, atr=True)
+        cmds.headsUpDisplay('rightBottomHUD', section=9, block=1, blockSize='medium', dataFontSize='large',
+                       command=self.getFrame, atr=True)
+
+    def removeHUD(self):
+        headsUpDisplayList = cmds.headsUpDisplay(lh=True)
+        for headDisplay in headsUpDisplayList:
+            cmds.headsUpDisplay(headDisplay, rem=True)
