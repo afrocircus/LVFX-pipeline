fa75f3bfe529f38f801b361071e5f224985cf0bd
afrocircus <natasha.kelkar@gmail.com>
2016-12-08 12:53:29 +0200

    validates 1 fur grp and 1 mesh grp

--- pyblish-plugins/modeling/validate_meshes.py
+++ pyblish-plugins/modeling/validate_meshes.py
@@ -9,17 +9,24 @@ class ValidateMeshes(pyblish.api.InstancePlugin):
     hosts = ['maya']
     families = ['scene']
     label = 'Validate Meshes'
+    print "running"
 
     def process(self, instance):
 
         assemblies = pymel.core.ls(assemblies=True)
         modelInstances = []
         for each in assemblies:
-            if any(ext in each.name().lower() for ext in ['grp']):
+            if any(ext in each.name().lower() for ext in ['grp', 'fur']):
                 modelInstances.append(each.name())
 
-        if len(modelInstances) > 1:
-            self.log.error('There should be only one parent group in the scene')
+        if len(modelInstances) == 2:
+            furCount = 0
+            for each in modelInstances:
+                if 'fur' in each.lower():
+                    furCount += 1
+
+        if len(modelInstances) > 2 or modelInstances == 0 or furCount != 1:
+            self.log.error('Invalid groups in the scene.')
             raise pyblish.api.ValidationError
 
-        instance.set_data('model', value=modelInstances[0])
+        instance.set_data('model', value=modelInstances)
