a5c4d7f58f5d677ede1e99d103002eda6c256117
afrocircus <natasha.kelkar@gmail.com>
2016-12-09 15:26:13 +0200

    validates image plane

--- pyblish-plugins/layout/validate_camera.py
+++ pyblish-plugins/layout/validate_camera.py
@@ -15,4 +15,10 @@ class ValidateCamera(pyblish.api.InstancePlugin):
         if len(cameraNode) == 0:
             self.log.error('Valid renderCam not found.')
             raise pyblish.api.ValidationError
+
+        camShapeNode = cmds.listRelatives(cameraNode, s=True, c=True)[0]
+        sourceConnections = cmds.listConnections(camShapeNode + '.imagePlane',
+                                                 source=True, type='imagePlane') or []
+        if len(sourceConnections) > 0:
+            instance.set_data('image_planes', value=sourceConnections)
         self.log.info('renderCam validated')
