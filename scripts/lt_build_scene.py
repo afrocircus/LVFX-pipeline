e6edb66373ddc54bc6856c93d6513ef4df280109
afrocircus <natasha.kelkar@gmail.com>
2016-12-13 18:36:37 +0200

    handles lighting updates

--- scripts/lt_build_scene.py
+++ scripts/lt_build_scene.py
@@ -3,6 +3,8 @@ import os
 import ftrack
 import argparse
 import glob
+import re
+import shutil
 import maya.cmds as cmds
 import maya.standalone
 
@@ -65,13 +67,22 @@ def getModelingRef(rigFile):
     return modelRef
 
 
-def buildScene(filepath, cameraFile, envFile, animMeta):
-    cmds.file(new=True)
-    cmds.file(rename=filepath)
+def buildScene(cameraFile, envFile, animMeta):
     if isValidFile(envFile):
-        cmds.file(envFile, r=True)
+        try:
+            # Check if env file is already referenced.
+            cmds.referenceQuery(envFile, filename=True)
+        except RuntimeError:
+            cmds.file(envFile, r=True)
+
     if isValidFile(cameraFile):
-        cmds.AbcImport(cameraFile, mode='import')
+        cameraNodes = cmds.ls('renderCam')
+        if len(cameraNodes) == 1:
+            cameraNode = cameraNodes[0]
+            cmds.AbcImport(cameraFile, connect=cameraNode, mode='import', fitTimeRange=True)
+        else:
+            cmds.AbcImport(cameraFile, mode='import')
+        print "camera imported"
 
     for char in animMeta:
         if 'ref' in animMeta[char]:
@@ -81,7 +92,11 @@ def buildScene(filepath, cameraFile, envFile, animMeta):
             else:
                 modelRef = getModelingRef(rigFile)
             if modelRef != '':
-                cmds.file(modelRef, r=True, namespace='char_%s' % char)
+                try:
+                    # Check if char file is already referenced.
+                    cmds.referenceQuery(modelRef, filename=True)
+                except RuntimeError:
+                    cmds.file(modelRef, r=True, namespace='char_%s' % char)
                 if 'publish' in animMeta[char] and 'mayaNode' in animMeta[char]:
                     print animMeta[char]
                     cmds.AbcImport(animMeta[char]['publish'], connect=animMeta[char]['mayaNode'],
@@ -89,6 +104,36 @@ def buildScene(filepath, cameraFile, envFile, animMeta):
                     print 'asset import: ' + animMeta[char]['publish']
 
     cmds.file(save=True, type='mayaBinary', force=True)
+
+
+def version_get(string, prefix, suffix=None):
+    """Extract version information from filenames.  Code from Foundry's nukescripts.version_get()"""
+
+    if string is None:
+        raise ValueError, "Empty version string - no match"
+
+    regex = "[/_.]" + prefix + "\d+"
+    matches = re.findall(regex, string, re.IGNORECASE)
+    if not len(matches):
+        msg = "No \"_" + prefix + "#\" found in \"" + string + "\""
+        raise ValueError, msg
+    return matches[-1:][0][1], re.search("\d+", matches[-1:][0]).group()
+
+
+def getLatestVersion(taskDir):
+    publishFile = ''
+    lightingFiles = [f for f in glob.glob(os.path.join(taskDir, '*_v[0-9]*.mb'))]
+    maxVersion = 1
+    if lightingFiles:
+        for f in lightingFiles:
+            try:
+                version = int(version_get(f, 'v')[1])
+            except ValueError:
+                continue
+            if version >= maxVersion:
+                publishFile = f
+                maxVersion = version
+    return maxVersion
 
 
 def main(argv):
@@ -112,7 +157,23 @@ def main(argv):
 
     filename = '%s_v01.mb' % shot.getName()
     filepath = os.path.join(taskDir, filename)
-    buildScene(filepath, shotCam, envFile, animMeta)
+
+    if os.path.exists(filepath):
+        version = getLatestVersion(taskDir)
+        oldFile = '%s_v%02d.mb' % (shot.getName(), version)
+        newFile = '%s_v%02d.mb' % (shot.getName(), version+1)
+        oldFilePath = os.path.join(taskDir, oldFile)
+        newFilePath = os.path.join(taskDir, newFile)
+        shutil.copyfile(oldFilePath, newFilePath)
+        cmds.file(newFilePath, open=True)
+        metadata = task.getMeta()
+        metadata['filename'] = newFilePath
+        task.setMeta(metadata)
+    else:
+        cmds.file(new=True)
+        cmds.file(rename=filepath)
+
+    buildScene(shotCam, envFile, animMeta)
 
     cmds.quit()
     os._exit(0)
