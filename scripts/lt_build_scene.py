0b3fddd8794822e0fe54f9d265d80cc9199f7eeb
afrocircus <natasha.kelkar@gmail.com>
2017-01-31 11:13:02 +0200

    bakes char file then references it into the lighting scene

--- scripts/lt_build_scene.py
+++ scripts/lt_build_scene.py
@@ -23,6 +23,7 @@ def getTaskByName(shot, taskName):
 def getCharPublishFile(shot):
     animMeta = {}
     animTask = getTaskByName(shot, 'animation')
+    version = None
     if animTask:
         meta = animTask.getMeta()
         refs = [ (key, meta[key]) for key in meta.keys() if 'ref' in key ]
@@ -35,17 +36,22 @@ def getCharPublishFile(shot):
             mayaKey = 'mayanode_%s' % charname
             if mayaKey in meta:
                 animMeta[charname]['mayaNode'] = meta[mayaKey]
-    return animMeta
+        if 'version' in meta:
+            version = meta['version']
+    return animMeta, version
 
 
 def getEnvPublishFile(shot):
     layoutTask = getTaskByName(shot, 'layout')
+    version = None
     envFile = None
     if layoutTask:
         metadata = layoutTask.getMeta()
         if 'publish_env_file' in metadata:
             envFile = metadata['publish_env_file']
-    return envFile
+        if 'version' in metadata:
+            version = metadata['version']
+    return envFile, version
 
 
 def isValidFile(filepath):
@@ -67,7 +73,14 @@ def getModelingRef(rigFile):
     return modelRef
 
 
-def buildScene(cameraFile, envFile, animMeta):
+def buildScene(cameraFile, envFile, charFile):
+    '''
+    Build the final lighting scene file.
+    :param cameraFile: Camera alembic file to import
+    :param envFile:  Published layout env file to reference
+    :param charFile: Baked char file to reference
+    :return:
+    '''
     if isValidFile(envFile):
         try:
             # Check if env file is already referenced.
@@ -75,33 +88,21 @@ def buildScene(cameraFile, envFile, animMeta):
         except RuntimeError:
             cmds.file(envFile, r=True)
 
+    if isValidFile(charFile):
+        try:
+            # Check if env file is already referenced.
+            cmds.referenceQuery(charFile, filename=True)
+        except RuntimeError:
+            cmds.file(charFile, r=True)
+
     if isValidFile(cameraFile):
+        # Check if camera exists in scene
         cameraNodes = cmds.ls('renderCam')
         if len(cameraNodes) == 1:
+            # Camara exists, delete and re-import alembic
             cameraNode = cameraNodes[0]
-            cmds.AbcImport(cameraFile, connect=cameraNode, mode='import', fitTimeRange=True)
-        else:
-            cmds.AbcImport(cameraFile, mode='import')
-        print "camera imported"
-
-    for char in animMeta:
-        if 'ref' in animMeta[char]:
-            rigFile = animMeta[char]['ref']
-            if 'modeling' in rigFile:
-                modelRef = rigFile
-            else:
-                modelRef = getModelingRef(rigFile)
-            if modelRef != '':
-                try:
-                    # Check if char file is already referenced.
-                    cmds.referenceQuery(modelRef, filename=True)
-                except RuntimeError:
-                    cmds.file(modelRef, r=True, namespace='char_%s' % char)
-                if 'publish' in animMeta[char] and 'mayaNode' in animMeta[char]:
-                    print animMeta[char]
-                    cmds.AbcImport(animMeta[char]['publish'], connect=animMeta[char]['mayaNode'],
-                                   mode='import', fitTimeRange=True)
-                    print 'asset import: ' + animMeta[char]['publish']
+            cmds.delete(cameraNode)
+        cmds.AbcImport(cameraFile, mode='import')
 
     cmds.file(save=True, type='mayaBinary', force=True)
 
@@ -121,6 +122,11 @@ def version_get(string, prefix, suffix=None):
 
 
 def getLatestVersion(taskDir):
+    '''
+    Get the max lighting file version in the folder
+    :param taskDir: Lighting task folder on disk
+    :return: maxVersion
+    '''
     publishFile = ''
     lightingFiles = [f for f in glob.glob(os.path.join(taskDir, '*_v[0-9]*.mb'))]
     maxVersion = 1
@@ -134,6 +140,72 @@ def getLatestVersion(taskDir):
                 publishFile = f
                 maxVersion = version
     return maxVersion
+
+
+def getCharBakeVersion(envVer, animVer, metadata):
+    '''
+    Get the char bake version based on the animation and layout publish versions
+    :param envVer: Latest Layout Publish Version
+    :param animVer: Latest Animation Publish Version
+    :param metadata: Lighting task metadata
+    :return: charVersion: Baked char version
+    '''
+    taskAnimVer = taskEnvVer = charVersion = None
+
+    if 'anim_version' in metadata:
+        taskAnimVer = metadata['anim_version']
+    if 'env_version' in metadata:
+        taskEnvVer = metadata['env_version']
+    if 'char_version' in metadata:
+        charVersion = int(metadata['char_version'].split('v')[-1])
+
+    if not charVersion:
+        charVersion = 1
+    elif taskEnvVer != envVer or taskAnimVer != animVer:
+        # If there is a new layout or animation publish version, char bake version changes
+        charVersion += 1
+    return charVersion
+
+
+def buildCharFile(taskDir, animMeta, version):
+    '''
+    Build a char file containing the baked animation.
+    :param taskDir: Lighting folder on disk
+    :param animMeta: Animation task metadta
+    :param version: Version of the baked char file
+    :return: charFile: Baked char file path
+    '''
+    bakedDir = os.path.join(taskDir, 'baked')
+    charFile = os.path.join(bakedDir, 'char.mb')
+    if os.path.exists(charFile):
+        os.remove(charFile)
+    versionDir = os.path.join(bakedDir, 'v%02d' % version)
+    if not os.path.exists(versionDir):
+        os.makedirs(versionDir)
+    charRefFile = os.path.join(versionDir, 'char.mb')
+    if os.path.exists(charRefFile):
+        os.remove(charRefFile)
+    cmds.file(new=True, f=True)
+    cmds.file(rename='%s' % charRefFile)
+
+    for char in animMeta:
+        if 'ref' in animMeta[char]:
+            rigFile = animMeta[char]['ref']
+            if 'modeling' in rigFile:
+                modelRef = rigFile
+            else:
+                modelRef = getModelingRef(rigFile)
+            if modelRef != '':
+                cmds.file(modelRef, i=True, namespace='char_%s' % char)
+                if 'publish' in animMeta[char] and 'mayaNode' in animMeta[char]:
+                    cmds.AbcImport(animMeta[char]['publish'], connect=animMeta[char]['mayaNode'],
+                                   mode='import', fitTimeRange=True)
+                    print 'asset import: ' + animMeta[char]['publish']
+
+    cmds.file(save=True, type='mayaBinary', force=True)
+    cmds.quit()
+    os.symlink(charRefFile, charFile)
+    return charFile
 
 
 def main(argv):
@@ -152,8 +224,14 @@ def main(argv):
     task = ftrack.Task(taskid)
     shot = task.getParent()
     shotCam = getRenderCam(shot)
-    envFile = getEnvPublishFile(shot)
-    animMeta = getCharPublishFile(shot)
+    envFile, envVersion = getEnvPublishFile(shot)
+    animMeta, animVersion = getCharPublishFile(shot)
+
+    metadata = task.getMeta()
+    # Get the char bake version
+    charVersion = getCharBakeVersion(envVersion, animVersion, metadata)
+    # Bake out the char file from latest animation publish
+    charFile = buildCharFile(taskDir, animMeta, charVersion)
 
     filename = '%s_v01.mb' % shot.getName()
     filepath = os.path.join(taskDir, filename)
@@ -165,17 +243,24 @@ def main(argv):
         oldFilePath = os.path.join(taskDir, oldFile)
         newFilePath = os.path.join(taskDir, newFile)
         shutil.copyfile(oldFilePath, newFilePath)
-        cmds.file(newFilePath, open=True)
-        metadata = task.getMeta()
+        cmds.file(filepath, open=True)
         metadata['filename'] = newFilePath
-        task.setMeta(metadata)
     else:
         cmds.file(new=True)
         cmds.file(rename=filepath)
+        metadata['filename'] = filepath
 
-    buildScene(shotCam, envFile, animMeta)
+    # build the scene
+    buildScene(shotCam, envFile, charFile)
 
     cmds.quit()
+    # Set metadata
+    metadata['charFile'] = charFile
+    metadata['char_version'] = 'v%02d' % charVersion
+    metadata['anim_version'] = animVersion
+    metadata['env_version'] = envVersion
+    task.setMeta(metadata)
+
     os._exit(0)
 
 
