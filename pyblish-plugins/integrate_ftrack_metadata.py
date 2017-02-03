41e86951f7df27dd09a95447362888f0fcd1d79d
afrocircus <natasha.kelkar@gmail.com>
2017-02-03 16:59:27 +0200

    if animation status is approved, then set status to ready for lighting.

--- pyblish-plugins/integrate_ftrack_metadata.py
+++ pyblish-plugins/integrate_ftrack_metadata.py
@@ -13,6 +13,7 @@ class IntegrateFtrackMeta(pyblish.api.InstancePlugin):
 
     hosts = ['maya']
     version = (0, 1, 0)
+    optional = True
 
     def process(self, instance):
 
@@ -23,11 +24,16 @@ class IntegrateFtrackMeta(pyblish.api.InstancePlugin):
 
         try:
             task.set('description', 'Published on %s' % instance.context.data['date'])
-        except:
+        except Exception:
             self.log.warn('Unable to set task description.')
 
         if 'renderCam' in metadata:
             shot = task.getParent()
             shot.set('shot_cam', metadata['renderCam'])
+
+        if task.getName().lower() == 'animation':
+            if task.getStatus().getName() == 'Approved':
+                status = ftrack.Status('Ready for Lighting')
+                task.setStatus(status)
 
         self.log.info('Integrated metadata with Ftrack')
